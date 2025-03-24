486884ae55aa2f3e52f58f1b8f0e2cae54601c0a
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2025-03-24 19:11:13 +0300

    rename main file to the first lesson file.

---
+++ lesson_1.py
@@ -0,0 +1,38 @@
+import telebot
+import webbrowser
+
+bot = telebot.TeleBot('7107134984:AAHp_9daxp7FGgY5LgXTPTI6ZfK2ymqSc44')
+
+
+@bot.message_handler(commands=['site', 'website'])
+def site(message):
+    webbrowser.open('https://vk.com/creator_my_life')
+
+
+@bot.message_handler(commands=['start', 'main', 'hello'])
+def main(message):
+    bot.send_message(message.chat.id, f'Привет, '
+                                      f'{message.from_user.first_name} '
+                                      f'{message.from_user.last_name}!')
+
+
+@bot.message_handler(commands=['help'])
+def main(message):
+    bot.send_message(message.chat.id, '<b>Help</b> '
+                                      '<em><u>information</u></em>',
+                     parse_mode='html')
+
+
+@bot.message_handler()
+def info(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, f'Привет, '
+                                          f'{message.from_user.first_name} '
+                                          f'{message.from_user.last_name}!')
+    elif message.text.lower() == 'id':
+        bot.reply_to(message, f'ID: {message.from_user.id}')
+
+
+bot.polling(non_stop=True)
+
+# bot.infinity_polling()
