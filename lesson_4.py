2ab0c892802b0dc752d3a7c11e9700d8707074f0
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2025-03-25 20:16:39 +0300

    save function for weather from the fourth lesson in file.

---
+++ lesson_4.py
@@ -0,0 +1,32 @@
+import telebot
+import requests
+import json
+
+bot = telebot.TeleBot('7107134984:AAHp_9daxp7FGgY5LgXTPTI6ZfK2ymqSc44')
+API = 'c070b0e8468add9b7616dfc84097d8d9'
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Привет! Напиши название своего города:')
+
+
+@bot.message_handler(content_types=['text'])
+def get_weather(message):
+    city = message.text.strip().lower()
+    res = requests.get(
+        f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid='
+        f'{API}&units=metric')
+    if res.status_code == 200:
+        data = json.loads(res.text)
+        temp = data["main"]["temp"]
+        bot.reply_to(message, f'Сейчас погода в городе {city.capitalize()}:'
+                              f' {temp} градусов по цельсию.')
+        image = 'clear.png' if temp > 5.0 else 'partly_cloudy.png'
+        file = open('./' + image, 'rb')
+        bot.send_photo(message.chat.id, file)
+    else:
+        bot.reply_to(message, 'Такого города нет.')
+
+
+bot.polling(non_stop=True)
