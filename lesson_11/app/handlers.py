20b4a77c6a65f90b34dc07a625a4a15361e99cca
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2025-04-01 08:07:48 +0300

    save func with fsmcontext in eleventh lesson dir

--- lesson_11/app/handlers.py
+++ lesson_11/app/handlers.py
@@ -1,9 +1,17 @@
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
+
 import app.keyboards as kb
 
 router = Router()
+
+
+class Reg(StatesGroup):
+    name = State()
+    number = State()
 
 
 @router.message(CommandStart())
@@ -40,6 +48,29 @@ async def get_photo_new(message: Message):
 async def catalog(callback: CallbackQuery):
     await callback.answer('Вы выбрали каталог')
     await callback.message.edit_text('Привет!', reply_markup=await
-                                    kb.inline_cars())
+    kb.inline_cars())
     # await callback.message.answer('Привет!', reply_markup=await
     #                               kb.inline_cars())
+
+
+@router.message(Command('reg'))
+async def get_reg(message: Message, state: FSMContext):
+    await state.set_state(Reg.name)
+    await message.answer('Введите ваше имя:')
+
+
+@router.message(Reg.name)
+async def reg_two(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await state.set_state(Reg.number)
+    await message.answer('Введите номер телефона:')
+
+
+@router.message(Reg.number)
+async def two_three(message: Message, state: FSMContext):
+    await state.update_data(number=message.text)
+    data = await state.get_data()
+    await message.answer(f'Регистрация завершена.'
+                         f'\nИмя: {data["name"]}'
+                         f'\nНомер: {data["number"]}')
+    await state.clear()
